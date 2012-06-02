fa38f82b91418582495179b5bf5405ab3f2b59ef
okapies <okapies@gmail.com>
2012-06-03 02:15:37 +0900

    rename '3DES' to 'TripleDES'.

--- src/main/scala/com/github/okapies/finagle/gntp/Encryption.scala
+++ src/main/scala/com/github/okapies/finagle/gntp/Encryption.scala
@@ -18,12 +18,12 @@ object EncryptionAlgorithm {
 
   case object DES extends EncryptionAlgorithm { val name = "DES" }
 
-  case object _3DES extends EncryptionAlgorithm { val name = "3DES" }
+  case object TripleDES extends EncryptionAlgorithm { val name = "3DES" }
 
   private val names = immutable.Map(
     (AES.name, AES),
     (DES.name, DES),
-    (_3DES.name, _3DES)
+    (TripleDES.name, TripleDES)
   )
 
   def withName(s: String): EncryptionAlgorithm = names(s)
